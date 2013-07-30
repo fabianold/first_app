f66948cdfb0288411b2a2cf96bc97caf36391c44
Fabiano Teixeira <fabiano@Padawan.(none)>
2013-07-30 13:48:40 -0300

    my first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial: primeira aplicação
+
+Este é o primeiro pedido de
+[* Ruby on Rails Tutorial *] (http://railstutorial.org/)
+por [Michael Hartl] (http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '0a5fa8faba4bcb953a93981ee0050f826c08427faa41216ebdf42c0572ac71407b4f88763e0aea76dfd6681d20dd167e93a5cdfadf7f23cfae150ea9ba728edd'
